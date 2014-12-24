cbafcca80d25d9f6cea04b7d219ba89d0ec7022d
Daniel Barker <djbarker@users.noreply.github.com>
2014-12-24 10:01:16 +0000

    GetTemp now writes an average over several readings to improve digital nature of readings.

--- GetTemp.py
+++ GetTemp.py
@@ -6,19 +6,20 @@ from datetime import datetime
 from time import sleep
 import MySQLdb as mdb
 
-def getTemp():
-    p = Popen(['/opt/vc/bin/vcgencmd','measure_temp'],stdout=PIPE)
-    stdout = p.communicate()
-    tmp = float(stdout[0].split('=')[1].split('\'')[0])
-
-    #p = Popen(['cat','/sys/class/thermal/thermal_zone0/temp'],stdout=PIPE)
-    #tmp = float(p.communicate()[0])/1000.0
+def getTemp(N):
+    #p = Popen(['/opt/vc/bin/vcgencmd','measure_temp'],stdout=PIPE)
+    #stdout = p.communicate()
+    #tmp = float(stdout[0].split('=')[1].split('\'')[0])
+    tmps = []
+    for i in range(N):
+        p = Popen(['cat','/sys/class/thermal/thermal_zone0/temp'],stdout=PIPE)
+        tmp = float(p.communicate()[0])/1000.0
+        tmps.append( tmp )
+        sleep(1)
     
-    return tmp
+    return sum(tmps)/float(N)
 
-with open('static/temps.tsv','w') as f:
-    f.write('date\tclose\n')
-
+sampling_int = 10
 logging_int = 60. # once a minute
 if len(sys.argv)>1:
     logging_int = float(sys.argv[1])
@@ -27,11 +28,11 @@ while True:
     try:
         with mdb.connect('localhost','root','raspberry','temps') as con:
             date_str = datetime.now().strftime('%y-%m-%d %H:%M:%S')
-            sql = "INSERT INTO tblTemps VALUES ('%s',%f);"%(date_str,getTemp())
+            sql = "INSERT INTO tblTemps VALUES ('%s',%f);"%(date_str,getTemp(sampling_int))
             con.execute(sql)
     except mdb.Error, e:
         print('MySQL Error %d: %s'%(e.args[0],e.args[1]))
 
-    sleep(logging_int);
+    sleep(logging_int-float(sampling_int));
 
 
