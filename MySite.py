4169855cec8af686aa6f003c6db47ec63cf7711b
Daniel Barker <djbarker@users.noreply.github.com>
2014-12-29 16:13:02 +0000

    Uses HTML5 data attributes to specify different number of interpolation points for different lookbacks.

--- MySite.py
+++ MySite.py
@@ -87,4 +87,5 @@ def chart():
     return render_template('chart.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    #app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0')
